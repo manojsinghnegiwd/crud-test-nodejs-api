16b3428a71e781a4267de4f4f252b05d9ad8727a
Manoj Singh Negi <justanothermanoj@gmail.com>
2019-10-11 00:08:03 +0530

    branch-2: integrated swagger

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const express = require('express')
 const app = express()
+const swaggerUi = require('swagger-ui-express')
 const mongoose = require('mongoose')
+
+const swaggerDocument = require('./swagger.json')
 
 mongoose.connect('mongodb://db:27017/test', { useNewUrlParser: true })
   .then(() => console.log('I am connected'))
   .catch(error => console.log(error));
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.get('/', (_, res) => {
 
